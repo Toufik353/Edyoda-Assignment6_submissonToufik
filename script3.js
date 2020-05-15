fe5262eac6062d46f4a803beacbd5dea5bc2a166
Toufik353 <60398527+Toufik353@users.noreply.github.com>
2020-05-16 00:35:15 +0530

    Update script3.js

--- script3.js
+++ script3.js
@@ -99,11 +99,10 @@ const cards = [
    var text2 = document.createTextNode(card_description);
     para1.appendChild(text2);
    cardDiv2.appendChild(para1);
-  cardDiv0.appendChild(cardDiv2);
-  
     cardDiv1.appendChild(cardDiv2);
-   
-    return cardDiv0;}
+    cardDiv0.appendChild(cardDiv2);
+    return cardDiv0;
+  }
     
   
   
